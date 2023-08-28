6389bec8282b67b44ca248ed2bf25855a53863bf
Lomonoga <trintsa@mail.ru>
2023-08-29 02:48:31 +0500

    test work

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-
-def print_hi(name):
-    print(f'Hi, {name}')
+import flet as ft
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main(page: ft.Page):
+
+    def add_text(text: str):
+        if text_field.value == "": return
+        box = ft.Text(value=text_field.value, size=16, width=350)
+        second_row = ft.Row(controls=[box], alignment=ft.MainAxisAlignment.START)
+        page.add(second_row)
+
+    page.window_width = 500
+    page.window_height = 700
+    page.window_resizable = False
+
+    page.bgcolor = '#45FFCA'
+    page.title = 'by Lomonoga'
+    page.window_maximizable = False
+
+    text_field = ft.TextField(border_radius=25,
+                              label="Введи сюда что то сука!!!",
+                              width=300,
+                              label_style=ft.TextStyle(size=20))
+
+    button = ft.ElevatedButton(text='ЖМИ', on_click=add_text)
+    first_row = ft.Row(controls=[text_field, button], alignment=ft.MainAxisAlignment.SPACE_AROUND)
+    page.add(first_row)
+
+
+ft.app(target=main)
